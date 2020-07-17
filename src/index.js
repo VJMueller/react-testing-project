dc3b89a82d4e2812095c1a0f1ab91864eaccab48
vmueller <vmueller@vmuellers-MBP.fritz.box>
2020-07-17 12:54:58 +0200

    Added searchable product data table from Thinking in React

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./components/Button"
+import FilterableProductTable from "./components/FilterableProductTable"
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 function Root() {
   return (
     <div>
       "hello world"
       <Button />
+      <FilterableProductTable products={PRODUCTS} />,
     </div>
-  )
+  );
 };
 
 // Render the Root element into the DOM
